c58cd28d775d6777db93cc9e96dbd9984477eed5
[01;31m[K"name"[m[K: "Dylan Yang <[01;31m[K"email"[m[K: null>
2017-07-29 18:32:43 -0400

    Include react-native-camera

--- index.ios.js
+++ index.ios.js
@@ -3,14 +3,17 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View, 
-  Image
+  View,
+  Image,
+  Dimensions,
+  TouchableHighlight,
 } from 'react-native';
 import axios from 'axios';
 import Recipe from './components/recipeList/Recipe'
 import List from './components/recipeList/List'
 import Login from './components/login/Login'
 import Registration from './components/register/Registration'
+import Camera from 'react-native-camera';
 
 export default class SeeFood extends Component {
 
@@ -28,15 +31,27 @@ export default class SeeFood extends Component {
         this.setState({ data : response.data })
       })
   }
+  clickedme(){
+    alert("was touched");
+  }
 
   render() {
-
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>List of recipes</Text>
         <List recipes={this.state.data}/>
         <Login />
         <Registration />
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}>
+          <TouchableHighlight onPress={this.clickedme.bind(this)}>
+              <View></View>
+          </TouchableHighlight>
+        </Camera>
       </View>
     );
   }
@@ -47,8 +62,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     backgroundColor: '#E0E0E0'
-  }
-  
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+
 });
 
 AppRegistry.registerComponent('SeeFood', () => SeeFood);
