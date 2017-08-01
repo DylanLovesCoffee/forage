b0d8d22feb699ad0f85e46757b7e84242d0a061f
[01;31m[K"name"[m[K: "Dylan Yang <[01;31m[K"email"[m[K: null>
2017-08-01 17:25:32 -0400

    Remove PhotoSelector from UserProfile

--- components/camera/AppCamera.js
+++ components/camera/AppCamera.js
@@ -5,29 +5,78 @@ import {
   Text,
   View,
   Image,
-  Dimensions,
+  CameraRoll,
   TouchableHighlight,
 } from 'react-native';
 import axios from 'axios';
 import Camera from 'react-native-camera';
+import Share from 'react-native-share';
+import RNFetchBlob from 'react-native-fetch-blob';
+import { API_KEY } from 'react-native-dotenv'
 
 export default class AppCamera extends Component {
+  constructor(){
+    super();
+    this.state = {
+      photos: [],
+    }
+  }
 
   static navigationOptions = {
     title: "Scan Your Groceries"
   };
 
-  clickedme = () => {
-    alert("was touched");
-  }
-
   takePicture() {
     const options = {};
-    //options.location = ...
     this.camera.capture({metadata: options})
-      .then((data) => console.log(data))
-      .catch(err => console.error(err));
+    .then((data) => console.log(data))
+    .catch(err => console.error(err));
   }
+
+  _getPhotos = () => {
+    CameraRoll.getPhotos({
+      first: 1,
+      assetType: 'Photos'
+    })
+    .then(response =>
+      this.setState({photos: response.edges})
+    )
+  }
+
+
+  share = () => {
+    let image = this.state.photos[0].node.image.uri
+    RNFetchBlob.fs.readFile(image, 'base64')
+    .then((data) => {
+      console.log("shared")
+      console.log(data)
+      this.callClarifaiBase(data)
+    })
+  }
+
+  callClarifaiBase(base) {
+    fetch("https://api.clarifai.com/v2/models/bd367be194cf45149e75f01d59f77ba7/outputs", {
+      method: "POST",
+      headers: {
+        "Authorization": "Key " + API_KEY,
+        "Content-Type": "application/json",
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        "inputs": [
+          { "data":
+            { "image":
+              { "base64": base }
+            }
+          }
+        ]
+      })
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+       return console.log(responseJson);
+     })
+   }
 
   render() {
     return(
@@ -38,7 +87,7 @@ export default class AppCamera extends Component {
           }}
           style={styles.preview}
           aspect={Camera.constants.Aspect.fill}>
-          <TouchableHighlight onPress={this.clickedme.bind(this)}>
+          <TouchableHighlight onPress={this._getPhotos.bind(this)}>
               <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[SCAN FOOD]</Text>
           </TouchableHighlight>
         </Camera>
--- components/profile/UserProfile.js
+++ components/profile/UserProfile.js
@@ -29,7 +29,6 @@ export default class UserProfile extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <PhotoSelection />
         <Text>Hello, {this.state.userFirstName}</Text>
         <ListView
         dataSource={this.state.recipeList}
