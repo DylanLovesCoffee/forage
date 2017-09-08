ef3eb271923991c321a5635a23cfd575cf95fabd
Dylan Yang <Dylan@Dylans-MacBook-Air.local>
2017-09-08 16:57:25 -0400

    Add git ignored files: API keys, Firebase auth

--- components/account/LoginForm.js
+++ components/account/LoginForm.js
@@ -10,26 +10,22 @@ import {
   Image,
   KeyboardAvoidingView
 } from 'react-native';
-import RegistrationForm from './RegistrationForm'
-import {firebaseRef} from '../services/Firebase'
+import RegistrationForm from './RegistrationForm';
+import * as firebase from 'firebase';
 import { Actions } from 'react-native-router-flux'
 import _ from 'lodash'
 import { StackNavigator } from 'react-navigation';
 import App from '../../App';
 
 export default class LoginForm extends Component {
-
-
-
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       email: '',
       password: ''
-    }
-
-    this._login = this._login.bind(this)
+    };
+    this._login = this._login.bind(this);
   }
 
   static navigationOptions = {
@@ -37,7 +33,7 @@ export default class LoginForm extends Component {
   };
 
   _login() {
-    firebaseRef.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
     .then(function(user){
       this.props.navigation.navigate('Profile')
     }.bind(this)).catch(function(error) {
@@ -47,45 +43,45 @@ export default class LoginForm extends Component {
   }
 
   render() {
-  return (
-    <View style={styles.color}>
-      <KeyboardAvoidingView behavior="padding">
-        <View style={styles.container}>
-          <View style={styles.form}>
-            <TextInput
-              autoCorrect={false}
-              autoCapitalize='none'
-              placeholder="email"
-              placeholderTextColor="black"
-              returnKeyType="next"
-              onChangeText = {(text) => this.setState({email: text})}
-              value={this.state.email}
-              onSubmitEditing={() => this.passwordInput.focus()}
-              keyboardType="email-address"
-              autoCorrect={false}
-              style={styles.input}
-            />
-            <TextInput
-              autoCorrect={false}
-              autoCapitalize='none'
-              placeholder="password"
-              placeholderTextColor="black"
-              onChangeText = {(text) => this.setState({password: text})}
-              value={this.state.password}
-              returnKeyType="go"
-              secureTextEntry
-              style={styles.input}
-              ref={(input) => this.passwordInput = input}
-            />
+    return (
+      <View style={styles.color}>
+        <KeyboardAvoidingView behavior="padding">
+          <View style={styles.container}>
+            <View style={styles.form}>
+              <TextInput
+                autoCorrect={false}
+                autoCapitalize='none'
+                placeholder="email"
+                placeholderTextColor="black"
+                returnKeyType="next"
+                onChangeText = {(text) => this.setState({email: text})}
+                value={this.state.email}
+                onSubmitEditing={() => this.passwordInput.focus()}
+                keyboardType="email-address"
+                autoCorrect={false}
+                style={styles.input}
+              />
+              <TextInput
+                autoCorrect={false}
+                autoCapitalize='none'
+                placeholder="password"
+                placeholderTextColor="black"
+                onChangeText = {(text) => this.setState({password: text})}
+                value={this.state.password}
+                returnKeyType="go"
+                secureTextEntry
+                style={styles.input}
+                ref={(input) => this.passwordInput = input}
+              />
+            </View>
+            <View style={styles.login}>
+              <TouchableOpacity style={styles.loginButtonContainer} onPress={this._login.bind(this)}>
+                <Text style={styles.loginButtonText}>LOGIN</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-          <View style={styles.login}>
-            <TouchableOpacity style={styles.loginButtonContainer} onPress={this._login.bind(this)}>
-              <Text style={styles.loginButtonText}>LOGIN</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </KeyboardAvoidingView>
-    </View>
+        </KeyboardAvoidingView>
+      </View>
     );
   }
 }
