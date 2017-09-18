9d977ee27f744070274a1578e56d76a533c09209
Dylan Yang <Dylan@Dylans-MacBook-Air.local>
2017-09-17 21:22:34 -0400

    Style details page. Display recipe title, ingredients, and instructions.

--- components/recipeList/RecipeDetails.js
+++ components/recipeList/RecipeDetails.js
@@ -11,15 +11,54 @@ import { StackNavigator } from 'react-navigation';
 export default class RecipeDetails extends Component {
 
   render() {
-    console.log("This is in the details")
     let recipe = this.props.navigation.state.params.recipeDetails
-    let instructionsArray = recipe.instructions.replace(/\s+/g,' ').trim().split('. ')
+    let instructions = recipe.analyzedInstructions[0].steps
+    let ingredients = recipe.extendedIngredients
     return(
-      <ScrollView>
-        {instructionsArray.map(function(line, i) {
-          return(<Text key={i}>{i + 1}. {line + '\n'}</Text>)
-        })}
+      <ScrollView style={styles.background}>
+        <Text style={styles.recipeTitle}>{recipe.title}</Text>
+        <View>
+          <Text style={styles.subTitle}>Ingredients</Text>
+          {ingredients.map(function(ingredient, i) {
+            return <Text key={i} style={styles.detailLine}>{ingredient.name}</Text>
+          })}
+        </View>
+        <View>
+          <Text style={styles.subTitle}>Directions</Text>
+          {instructions.map(function(line, i) {
+            return <Text key={i} style={styles.detailLine}>{line.step}</Text>
+          })}
+        </View>
       </ScrollView>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    padding: 20,
+    flexDirection: 'column',
+    backgroundColor: '#000000'
+  },
+  recipeTitle: {
+    textAlign: 'center',
+    color: '#FFFFFF',
+    fontSize: 25,
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  subTitle: {
+    textAlign: 'center',
+    color: '#FFFFFF',
+    fontSize: 15,
+    padding: 10,
+  },
+  detailLine: {
+    textAlign: 'left',
+    color: '#FFFFFF',
+    fontSize: 12,
+    padding: 10,
+  },
+
+})
